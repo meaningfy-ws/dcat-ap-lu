059012d6b383d0e6d3d0a60bf62ca8173495cefc
Rashif Ray Rahman <schiv@archlinux.org>
2025-08-13 14:39:25 +0600

    refactor: simplify the BDD feature testing approach
    Remove some redundancy leaving only the test case name, valid and
    invalid violation counts as user inputs. Return tuples throughout the
    test so as to facilitate validating both the valid and invalid cases.

--- tests/features/shacl/rdf_fragments.feature
+++ tests/features/shacl/rdf_fragments.feature
@@ -1,16 +1,12 @@
-Feature: Validate RDF fragments against SHACL shapes
+Feature: Validate RDF data fragments against SHACL shapes
 
-  Scenario Outline: Valid and invalid RDF fragments should conform accordingly
-    Given the valid RDF data fragment of <data_shape>
-    And the invalid RDF data fragment of <data_shape>
-    And the full shapes graph
-    When I validate the valid data against the shapes
-    And I validate the invalid data against the shapes
-    Then the valid result should conform to <expected_valid_result>
-    And the invalid result should conform to <expected_invalid_result>
-    And the valid result count should be <expected_valid_count>
-    And the invalid result count should be <expected_invalid_count>
+  Scenario Outline: Valid and invalid RDF data fragments should conform accordingly
+    Given the RDF data fragments of the SHACL test case <test_case>
+    And the full SHACL shapes graph
+    When I validate the valid and invalid data against the shapes
+    Then the valid data validation result should conform to <expected_valid_violation_count>
+    And the invalid data validation result should conform to <expected_invalid_violation_count>
 
     Examples:
-      | data_shape                       | expected_valid_result | expected_invalid_result | expected_valid_count | expected_invalid_count |
-      | dcat-Distribution-dcat-mediaType | True                  | False                   |                    0 |                      3 |
+      | test_case                        | expected_valid_violation_count | expected_invalid_violation_count |
+      | dcat-Distribution-dcat-mediaType |                              0 |                                3 |
--- tests/steps/shacl/test_rdf_framents.py
+++ tests/steps/shacl/test_rdf_framents.py
@@ -9,88 +9,74 @@ scenarios("../../features/shacl/rdf_fragments.feature")
 
 
 @given(
-    parsers.parse("the valid RDF data fragment of {data_shape}"),
-    target_fixture="valid_data_graph",
+    parsers.parse("the RDF data fragments of the SHACL test case {shacl_test_case}"),
+    target_fixture="test_data_graphs",
 )
-def get_valid_data_graph(data_shape):
-    return Graph().parse(
-        TEST_DATA_FOLDER / data_shape / f"{data_shape}_valid.{DEFAULT_RDF_FORMAT}",
+def get_test_data_graphs(shacl_test_case):
+    valid_data_graph = Graph().parse(
+        TEST_DATA_FOLDER
+        / shacl_test_case
+        / f"{shacl_test_case}_valid.{DEFAULT_RDF_FORMAT}",
         format=DEFAULT_RDF_FORMAT,
     )
-
-
-@given(
-    parsers.parse("the invalid RDF data fragment of {data_shape}"),
-    target_fixture="invalid_data_graph",
-)
-def get_invalid_data_graph(data_shape):
-    return Graph().parse(
-        TEST_DATA_FOLDER / data_shape / f"{data_shape}_invalid.{DEFAULT_RDF_FORMAT}",
+    invalid_data_graph = Graph().parse(
+        TEST_DATA_FOLDER
+        / shacl_test_case
+        / f"{shacl_test_case}_invalid.{DEFAULT_RDF_FORMAT}",
         format=DEFAULT_RDF_FORMAT,
     )
+    return valid_data_graph, invalid_data_graph
 
 
-@given("the full shapes graph", target_fixture="shapes_graph")
+@given("the full SHACL shapes graph", target_fixture="shapes_graph")
 def get_shapes_graph(full_shacl_shapes):
     return full_shacl_shapes
 
 
 @when(
-    "I validate the valid data against the shapes", target_fixture="valid_result_graph"
+    "I validate the valid and invalid data against the shapes",
+    target_fixture="validation_result_graphs",
 )
-def get_valid_result_graph(valid_data_graph, shapes_graph):
-    conforms, report_graph, _ = validate(valid_data_graph, shacl_graph=shapes_graph)
-    return conforms, report_graph
+def get_validation_result_graphs(test_data_graphs, shapes_graph):
+    valid_data_graph, invalid_data_graph = test_data_graphs
+    _, valid_report_graph, _ = validate(valid_data_graph, shacl_graph=shapes_graph)
+    _, invalid_report_graph, _ = validate(invalid_data_graph, shacl_graph=shapes_graph)
+    return valid_report_graph, invalid_report_graph
 
 
-@when(
-    "I validate the invalid data against the shapes",
-    target_fixture="invalid_result_graph",
+@then(
+    parsers.parse(
+        "the valid data validation result should conform to {expected_valid_violation_count:d}"
+    )
 )
-def get_invalid_result_graph(invalid_data_graph, shapes_graph):
-    conforms, report_graph, _ = validate(invalid_data_graph, shacl_graph=shapes_graph)
-    return conforms, report_graph
-
-
-@then(parsers.parse("the valid result should conform to {expected_valid_result}"))
-def assert_valid_result(valid_result_graph, expected_valid_result):
-    valid_result, report_graph = valid_result_graph
-    expected_result = expected_valid_result.lower() == "true"
-    assert (
-        valid_result == expected_result
-    ), f"SHACL validation failed:\n{report_graph.serialize()}"
-
-
-@then(parsers.parse("the invalid result should conform to {expected_invalid_result}"))
-def assert_invalid_result(invalid_result_graph, expected_invalid_result):
-    invalid_result, report_graph = invalid_result_graph
-    expected_result = expected_invalid_result.lower() == "true"
-    assert (
-        invalid_result == expected_result
-    ), f"SHACL validation failed:\n{report_graph.serialize()}"
-
-
-@then(parsers.parse("the valid result count should be {expected_valid_count:d}"))
-def assert_valid_result_count(ns, valid_result_graph, expected_valid_count):
-    _, report_graph = valid_result_graph
+def assert_valid_result_count(
+    ns, validation_result_graphs, expected_valid_violation_count
+):
+    valid_report_graph, _ = validation_result_graphs
 
     result_count = sum(
-        1 for _ in report_graph.subjects(ns.RDF.type, ns.SH.ValidationResult)
+        1 for _ in valid_report_graph.subjects(ns.RDF.type, ns.SH.ValidationResult)
     )
 
     assert (
-        result_count == expected_valid_count
-    ), f"SHACL validation failed:\n{report_graph.serialize()}"
+        result_count == expected_valid_violation_count
+    ), f"SHACL validation failed:\n{valid_report_graph.serialize()}"
 
 
-@then(parsers.parse("the invalid result count should be {expected_invalid_count:d}"))
-def assert_invalid_result_count(ns, invalid_result_graph, expected_invalid_count):
-    _, report_graph = invalid_result_graph
+@then(
+    parsers.parse(
+        "the invalid data validation result should conform to {expected_invalid_violation_count:d}"
+    )
+)
+def assert_invalid_result_count(
+    ns, validation_result_graphs, expected_invalid_violation_count
+):
+    _, invalid_report_graph = validation_result_graphs
 
     result_count = sum(
-        1 for _ in report_graph.subjects(ns.RDF.type, ns.SH.ValidationResult)
+        1 for _ in invalid_report_graph.subjects(ns.RDF.type, ns.SH.ValidationResult)
     )
 
     assert (
-        result_count == expected_invalid_count
-    ), f"SHACL validation failed:\n{report_graph.serialize()}"
+        result_count == expected_invalid_violation_count
+    ), f"SHACL validation failed:\n{invalid_report_graph.serialize()}"
